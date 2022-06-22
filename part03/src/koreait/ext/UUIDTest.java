9880a438abda2a12c1f794855f8810c9d963423e
Seonggu Han <everblackjoa52@gmail.com>
2022-06-22 15:34:15 +0900

    uuid테스트

---
+++ part03/src/koreait/ext/UUIDTest.java
@@ -0,0 +1,25 @@
+package koreait.ext;
+
+import java.util.UUID;
+
+public class UUIDTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String uuid = null;
+		//16진수 32자리 + 하이픈 기호 4개로 만들어지는 랜덤 문자열
+		for(int i = 0; i < 10; i++) {
+			uuid = UUID.randomUUID().toString();
+			System.out.println(uuid);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			uuid = UUID.randomUUID().toString();
+			System.out.println(uuid.replace("-", ""));
+		}
+		for(int i = 0; i < 10; i++) {
+			uuid = UUID.randomUUID().toString();
+			System.out.println(uuid.replace("-", "").substring(0,12));
+		}
+	}
+}
